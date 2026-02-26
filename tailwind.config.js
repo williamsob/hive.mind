b66ce147e1fda1efcb697c6df8eef6ee68a946f6
GnarlyDen <Dedobor76@gmail.com>
2026-02-26 14:36:35 +0000

    completed homepage

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './homepage.html',
+    './login.html',
+    './signup.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
