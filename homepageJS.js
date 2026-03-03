5b401008f363b2febd7d5f32601304d4f61f3318
GnarlyDen <Dedobor76@gmail.com>
2026-03-03 14:34:04 +0000

    unimportant

--- homepageJS.js
+++ homepageJS.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import * as fs from 'fs';
 
 fs.readFile('data/user_details.txt', 'utf8', (err, data) => {
     if (err){
@@ -8,3 +8,19 @@ fs.readFile('data/user_details.txt', 'utf8', (err, data) => {
 
     console.log(data);
 });
+
+
+console.log('done');
+let x = document.getElementById("study-group-container").childNodes;
+console.log(x);
+
+// const fs = require('fs');
+
+// fs.readFile('data/user_details.txt', 'utf8', (err, data) => {
+//     if (err){
+//         console.log("ff");
+//         return;
+//     }
+
+//     console.log(data);
+// });
